96de69d51602ae4490d78f1948f44dc7fefc18ea
cosmo2097 <zhangchao51@163.com>
2012-02-09 19:14:05 +0800

    update

--- maitianer-iphone/Authorization.m
+++ maitianer-iphone/Authorization.m
@@ -8,13 +8,6 @@
 
 #import "Authorization.h"
 
-@interface Authorization : NSObject {
-}
-
-+(void) deleteAuthorization;
-
-// Cookies
-+(void) setCookies:(NSArray*)cookies;
-+(NSArray*) cookies;
+@implementation Authorization
 
 @end
